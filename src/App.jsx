3dcd289a4ac714f56c8c0b710d3f7440642ffadc
sei-dev-git <seisport25@gmail.com>
2025-07-22 03:33:33 -1200

    Add: Basic App component

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to SEI SPORTS</h1>
+      <p>This is your home page. Site is now live and loading correctly!</p>
+    </div>
+  );
+}
+
+export default App;
